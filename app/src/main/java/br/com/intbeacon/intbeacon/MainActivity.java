99da256317bd0ef7081a9c800f258d9aa945d32f
Rafael Antonio Lucio <rafael.lucio@MacBook-Pro-de-administrador-3.local>
2017-06-01 21:56:06 -0300

    Add descriptions end Icons

--- app/src/main/java/br/com/intbeacon/intbeacon/MainActivity.java
+++ app/src/main/java/br/com/intbeacon/intbeacon/MainActivity.java
@@ -1,19 +1,15 @@
 package br.com.intbeacon.intbeacon;
 
-import android.app.Activity;
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity<T> extends AppCompatActivity {
 
-    long time = 10000;
+    long time = 3000;
     Class<T> clazz;
 
     @Override
@@ -21,36 +17,35 @@ public class MainActivity<T> extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         clazz = (Class<T>) FirstBeaconActivity.class;
-        notification(clazz, 001, "Vai curintia");
+        notification(clazz, 001, "Siga em Frente por 10 segundos");
+
         try {
             Thread.sleep(time);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
         clazz = (Class<T>) SecondBeaconActivity.class;
-        notification(clazz, 002, "Vixxxiii");
+        notification(clazz, 002, "Vire a direita e siga em frente por 10 segundos");
+
         try {
             Thread.sleep(time);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
         clazz = (Class<T>) ThirdyBeaconActivity.class;
-        notification(clazz, 003, "Vai curintia");
-        try {
-            Thread.sleep(time);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        notification(clazz, 003, "Você chegou a sala 502b");
     }
-
 
     public void notification(Class<T> activity, int id, String text) {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-        builder.setContentTitle("IntBeacon"+ id)
-                .setSmallIcon(R.mipmap.ic_launcher)
+        builder.setContentTitle("IntBeacon "+ id)
+                .setSmallIcon(R.drawable.ic_add_alert_black_24dp)
                 .setContentText(text);
 
         Intent intent = new Intent(MainActivity.this, activity);
