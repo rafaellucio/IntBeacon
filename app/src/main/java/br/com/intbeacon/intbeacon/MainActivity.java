2df3fa6a28ab45e8a2dbd73cb103a1bfceadfa1c
Rafael Antonio Lucio <rafael.lucio@MacBook-Pro-de-administrador-3.local>
2017-06-12 18:06:17 -0300

    Corrige manifest

--- app/src/main/java/br/com/intbeacon/intbeacon/MainActivity.java
+++ app/src/main/java/br/com/intbeacon/intbeacon/MainActivity.java
@@ -3,16 +3,19 @@ package br.com.intbeacon.intbeacon;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.os.RemoteException;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -20,15 +23,21 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
-import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.MonitorNotifier;
 import org.altbeacon.beacon.Region;
 
-public class MainActivity<T> extends FragmentActivity implements BeaconConsumer, OnMapReadyCallback {
+import java.util.ArrayList;
+
+public class MainActivity<T> extends FragmentActivity
+        implements BeaconConsumer,
+        OnMyLocationButtonClickListener,
+        OnMapReadyCallback {
 
     protected static final String TAG = "MonitoringActivity";
     private BeaconManager beaconManager;
+
     private GoogleMap map;
+    ArrayList markerPoints = new ArrayList();
 
     private static final LatLng FACULDADE = new LatLng(-23.599787, -46.676977);
     private static final String UUID_BEACON_01 = "0x699ebc80e1f311e39a0f";
@@ -36,7 +45,7 @@ public class MainActivity<T> extends FragmentActivity implements BeaconConsumer,
     private static final String UUID_BEACON_03 = "0x699ebc80e1f311e39a0f";
 
 
-    private static final long TIMEOUT = 10 * 1000;
+    private static final long TIMEOUT = 17 * 1000;
     Class<T> clazz;
 
     Handler handler = new Handler();
@@ -65,7 +74,6 @@ public class MainActivity<T> extends FragmentActivity implements BeaconConsumer,
         setContentView(R.layout.activity_main);
 
         beaconManager = BeaconManager.getInstanceForApplication(this);
-        //beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
         beaconManager.bind(this);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
@@ -138,9 +146,26 @@ public class MainActivity<T> extends FragmentActivity implements BeaconConsumer,
         map.getUiSettings().setTiltGesturesEnabled(true);
         map.getUiSettings().setZoomControlsEnabled(true);
         map.getUiSettings().setZoomGesturesEnabled(true);
+        map.setOnMyLocationButtonClickListener(this);
 
-        map.addMarker(new MarkerOptions().position(FACULDADE).title("Universidade Anhembi Morumbi"));
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+        }
+        map.setMyLocationEnabled(true);
+
+        MarkerOptions options = new MarkerOptions();
+        options.position(FACULDADE).title("Universidade Anhembi Morumbi");
+
+        map.addMarker(options);
+
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(FACULDADE, 15));
+
     }
 
     public void notification(Class<T> activity, int id, String text) {
@@ -179,5 +204,11 @@ public class MainActivity<T> extends FragmentActivity implements BeaconConsumer,
     public void dispatchThirdBeacon() {
         clazz = (Class<T>) ThirdyBeaconActivity.class;
         notification(clazz, 003, "Você chegou a sala 502b");
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "Minha localização", Toast.LENGTH_SHORT).show();
+        return false;
     }
 }
